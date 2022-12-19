36f2591459c0764f30f8009180ea5d37d85feef3
Rajeev <imrjvydv@gmail.com>
2022-12-19 14:53:56 +0530

    initial commit

---
+++ django/pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def hompepageview(request):
+    return HttpResponse ("this is rajeev yadav website the greatest man soon to be on the earth ")
+
